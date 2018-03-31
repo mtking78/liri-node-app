755df4653a7a16a7819e97a53f03667b922c1f55
mtking78 <mtking78@gmail.com>
2018-03-30 20:09:37 -0700

    Callback errors added to functions, doThis function calls all functions, all successful commands logged to log.txt.

--- liri.js
+++ liri.js
@@ -5,18 +5,9 @@ var inquirer = require("inquirer");
 var colors = require('colors');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-
-// Grab the fs package to handle read/write
 var fs = require("fs");
-
-// Code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
-    // Test to see if keys.js is exporting to liri.js by running "node .liri.js"
-    // Expected to print "this is loaded"
-    //*** The line below will print actual keys ***
-    // console.log(keys);
 
-// You should then be able to access your keys information like so:
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var omdb = (keys.omdb);
@@ -111,7 +102,6 @@ inquirer.prompt(
 //************FUNCTIONS************//
 
 // node liri.js my-tweets
-// This will show your last 20 tweets and when they were created at in your terminal/bash window.
 function myTweets() {
 
     var params = {screen_name: 'mtking78'};
@@ -125,6 +115,12 @@ function myTweets() {
                 console.log(tweets[i].text);
                 console.log(tweets[i].created_at.substring(0, 19));
                 console.log("");
+                // Append every full return to the log.txt file.
+                fs.appendFile("log.txt", "\n" + tweets[0].user.screen_name + "\n" + tweets[i].text + "\n" + tweets[i].created_at.substring(0, 19) + "\n", function(error){
+                    if (error) {
+                        console.log(error);
+                    }
+                });
             }
             console.log("**************************************");
             console.log("");
@@ -149,9 +145,15 @@ function spotifyThisSong (song) {
             console.log("");
             console.log("**************************************");
             console.log("");
+            // Append every full return to the log.txt file.
+            fs.appendFile("log.txt", "\n" + "Artist name: " + firstReturn.artists[0].name + "\nSong title: " + firstReturn.name + "\nPreview link at: " + firstReturn.external_urls.spotify + "\nAlbum title: " + firstReturn.album.name + "\n", function(error){
+                if (error) {
+                    console.log(error);
+                }
+            });
         } else {
             return console.log('An error occurred: ' + error);
-        }
+        };
     });
 }
 
@@ -160,22 +162,27 @@ function movieThis (movie) {
 
     var omdbUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + omdb.key;
 
-    // Then run a request to the OMDB API with the movie specified
     request(omdbUrl, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-
+            var info = JSON.parse(body);
             console.log("");
-            console.log('***** "' + (JSON.parse(body).Title) + '" *****');
-            console.log("Released in " + (JSON.parse(body).Year));
-            console.log("IMDB " + (JSON.parse(body).Ratings[0].Value));
-            console.log("Rotten Tomatoes " + (JSON.parse(body).Ratings[1].Value));
-            console.log("Produced in " + (JSON.parse(body).Country));
-            console.log("Language: " + (JSON.parse(body).Language));
-            console.log("Plot: " + (JSON.parse(body).Plot));
-            console.log("Actors: " + (JSON.parse(body).Actors));
+            console.log(('***** "' + info.Title + '" *****').inverse);
+            console.log("Released in " + info.Year);
+            console.log("IMDB " + info.Ratings[0].Value);
+            console.log("Rotten Tomatoes " + info.Ratings[1].Value);
+            console.log("Produced in " + info.Country);
+            console.log("Language: " + info.Language);
+            console.log("Plot: " + info.Plot);
+            console.log("Actors: " + info.Actors);
             console.log("");
             console.log("**************************************");
             console.log("");
+            // Append every full return to the log.txt file.
+            fs.appendFile("log.txt", "\n" + '***** "' + info.Title + '" *****' + "\nReleased in " + info.Year + "\nIMDB " + info.Ratings[0].value + "\nRotten Tomatoes " + info.Ratings[1].Value + "\nProduced in " + info.Country + "\nLanguage: " + info.Language + "\nPlot: " + info.Plot + "\nActors: " + info.Actors + "\n", function(error) {
+                if (error) {
+                    console.log(error);
+                };
+            });
         } else {
             console.log('An error occurred: ' + error);
         }
@@ -183,22 +190,29 @@ function movieThis (movie) {
 }
 
 //node liri.js do-what-it-says
-//Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-//Feel free to change the text in that document to test out the feature for other commands.
 function doThis () {
 
     fs.readFile("random.txt", "utf8", function(error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
+        var things = data.split(",");
+        var command = things[0];
+        var searchInput = things[1];
+
         if (!error) {
-            var things = data.split(",");
-            // Loop through the newly created array
+
             for (var i = 1; i < things.length; i++) {
-                // We will then print the contents of data
                 //console.log(things);
                 //console.log(things[i]);
-                spotifyThisSong(things[i]);
+                //spotifyThisSong(things[i]);
+                if (command === "my-tweets") {
+                    myTweets(searchInput);
+                } else if (command === "spotify-this-song") {
+                    spotifyThisSong(searchInput);
+                } else if (command === "movie-this") {
+                    movieThis(searchInput);
+                } else {
+                    return;
+                }
             }
         } else {
             return console.log(error);
